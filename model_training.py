b458d529187b969aa20c1725085ff5d20295d385
Henis Patel <henis.patel@ucalgary.ca>
2025-03-26 10:14:28 -0600

    added the model training file

---
+++ model_training.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+
+# 1. Load the dataset
+data = pd.read_csv('cleaned_breast_cancer_data.csv')
